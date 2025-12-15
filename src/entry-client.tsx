ec748236ea785a90c1dc5219bcecea8c8d9f9043
bibek1414 <bbhattarai770@gmail.com>
2025-12-15 11:47:11 +0545

    dsa

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import './index.css'
+import { StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 import App from './App'
 
-hydrateRoot(document.getElementById('root')!, <App />)
+hydrateRoot(
+  document.getElementById('root') as HTMLElement,
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
