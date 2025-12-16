11f00a6871fad24ff97b6c9824de0caef16fc04f
bibek1414 <bbhattarai770@gmail.com>
2025-12-16 13:26:11 +0545

    dasd

---
+++ src/Test.tsx
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <div>
+      <h1>Test Component (CLIENT FALLBACK)</h1>
+      <p>If you see this, SSR FAILED, and the client took over.</p>
+    </div>
+  )
+}
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import { renderToString } from 'react-dom/server'
-import App from './App'
+import App from './App' 
 
-export function render() {
-  return renderToString(<App />)
+export function render(_url: string) {
+  const html = renderToString(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+  return { html }
 }
