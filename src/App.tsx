3d309ca9986238936e5dc3c1333d29d753a40861
nepdora-nepal <nepdoranepal@gmail.com>
2025-12-17 17:54:37 +0545

    dfd

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App({ initialData = {} }: { initialData?: any }) {
 
   return (
     <main className="page">
-      <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/products" replace />} />
         <Route
