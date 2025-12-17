2798dfb7240f52a1e8c5f4eb3c5f990d28961960
bibek1414 <bbhattarai770@gmail.com>
2025-12-17 21:13:56 +0545

    dsa

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,22 +5,22 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <a to="/" className="navbar-logo">
+        <a href="/" className="navbar-logo">
           Glow Store
         </a>
         <ul className="navbar-menu">
           <li className="navbar-item">
-            <a to="/" className="navbar-a">
+            <a href="/" className="navbar-a">
               Home
             </a>
           </li>
           <li className="navbar-item">
-            <a to="/about" className="navbar-a">
+            <a href="/about" className="navbar-a">
               About
             </a>
           </li>
           <li className="navbar-item">
-            <a to="/products" className="navbar-a">
+            <a href="/products" className="navbar-a">
               Products
             </a>
           </li>
